6f715688ccab24c5cf885e1d2f9392781df82651
CountingMstar <mwh510@naver.com>
2023-04-23 23:51:14 +0900

    dataset

--- train.py
+++ train.py
@@ -74,9 +74,14 @@ backend = 'nccl' # 'nccl', 'gloo', etc.
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
 dtype = 'bfloat16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
 compile = False # use PyTorch 2.0 to compile the model to be faster
+
 # -----------------------------------------------------------------------------
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
+"""
+dataset = configurator.py의 데이터셋으로 변경
+"""
 exec(open('configurator.py').read()) # overrides from command line or config file
+
 config = {k: globals()[k] for k in config_keys} # will be useful for logging
 # -----------------------------------------------------------------------------
 
